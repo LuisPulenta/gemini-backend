db739495ea32ff8a735461d85ff493f2446a4e60
Luis Núñez <luisalbertonu@gmail.com>
2025-11-29 10:59:23 -0300

    Video 78

--- src/gemini/use-cases/image-generation.use-case.ts
+++ src/gemini/use-cases/image-generation.use-case.ts
@@ -1,7 +1,9 @@
-import { Content, createPartFromUri, GoogleGenAI } from '@google/genai';
+import { Content, ContentListUnion, createPartFromUri, GoogleGenAI, Modality } from '@google/genai';
 import { ChatPromptDto } from '../dtos/chat-prompt.dto';
 import { geminiUploadFiles } from '../helpers/gemini-upload-file';
 import { ImageGenerationDto } from '../dtos/image-generation.dto';
+
+import {v4 as uuidV4} from 'uuid';
 
 interface Options {
   model?: string;
@@ -20,17 +22,49 @@ export const imageGenerationUseCase = async (
   options?: Options,
 ):Promise<ImageGenerationResponse> => {
   const { prompt, files = [] } = imageGenerationDto;
+  
+
+  const contents: ContentListUnion = [
+    {text: prompt}
+  ]
+
   const uploadedFiles = await geminiUploadFiles(ai, files);
 
-  // const {
-  //   model = 'gemini-2.0-flash',
-  //   systemInstruction = `
-  //     Responde únicamente en español 
-  //     En formato markdown 
-  //     Usa negritas de esta forma __
-  //     Usa el sistema métrico decimal
-  // `,
-  // } = options ?? {};
+  uploadedFiles.forEach(file => {
+    contents.push(createPartFromUri(file.uri ?? '', file.mimeType ?? ''));
+  });
+
+  const {
+    model = 'gemini-2.0-flash-exp-image-generation',
+  } = options ?? {};
+
+  const response = await ai.models.generateContent({
+    model:model,
+    contents:contents,
+    config:{
+      responseModalities: [Modality.TEXT,Modality.IMAGE],
+    }
+  });
+
+  console.log(response);
+
+  let imageUrl = '';
+  let text = '';
+  const imageId = uuidV4();
+
+  for (const part of response.candidates?.[0]?.content?.parts ?? []){
+    if (part.text){
+      text =part.text;
+      continue;
+    }
+    if(!part.inlineData){
+      continue;
+    }
+    const imageData = part.inlineData.data!;
+    const buffer = Buffer.from(imageData,'base64');
+    console.log(buffer);
+  }
+  console.log({text});
 
   return {
     imageUrl:'xyz',
